c084c643e25910ed49c757329143ccdc7ed53a4e
marceesty <marceesty@gmail.com>
2022-10-03 15:54:51 +0100

    pages render handler model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/marceesty/learning_go_web/pkg/config"
+	"github.com/marceesty/learning_go_web/pkg/handlers"
+	"github.com/marceesty/learning_go_web/pkg/render"
 )
 
 const portNumber = ":8080"
 
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	var app config.AppConfig
 
-	fmt.Printf("Starting app on port %s", portNumber)
-	_ = http.ListenAndServe(portNumber, nil)
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Printf("Starting app on port %s\n", portNumber)
+	http.ListenAndServe(portNumber, nil)
 }
---
+++ cmd/web/templates/render2.go
@@ -0,0 +1,72 @@
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+var functions = template.FuncMap{
+
+}
+
+func RenderTemplate(w http.ResponseWriter, html string)  {
+
+	tc, err := CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, ok := tc[html]
+	if !ok {
+		log.Fatal("wait2")
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = t.Execute(buf, nil)
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+	}
+
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./teplates/*.page.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+		matches, err := filepath.Glob("./teplates/*.layout.html")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.html")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
+}
